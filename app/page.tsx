7be2b84ffa5c59f5f3843fef36f163c243c5074c
Akash Danda <mindstormakash@gmail.com>
2025-12-30 15:25:49 -0800

    resume

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from "../components/hero-section"
 import AboutSection from "../components/about-section"
 import ProjectsSection from "../components/projects-section"
 import ExperienceSection from "../components/experience-section"
-import ResumeSection from "../components/resume-section"
 import ContactSection from "../components/contact-section"
 import Footer from "../components/footer"
 
